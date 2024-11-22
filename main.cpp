51fb79257e714284d464a2103989064908ec9425
Maruf Rayhan <marufrayhan2002@gmail.com>
2024-11-22 23:13:30 +0600

    Slight Tuning

--- main.cpp
+++ main.cpp
@@ -32,6 +32,8 @@ int main()
     printSlowly("\t\t\t\t\t\t|_|  \\__,_|_|  |_| |_| |_/_/    \\_\\___/___/_|___/\\__|\n", 3);
     cout << endl;
     cout << endl;
+    printSlowly("\t\t\t\t\t\tLoading ... \n", 3);
+    cout << endl;
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BACKGROUND_RED | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
     printSlowly("                                                                                                        \n", 3);
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
